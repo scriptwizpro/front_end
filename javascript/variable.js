12fdd6a39544b67c47f91aeb6e58582fde62a412
Dev <androidsoloapps@gmail.com>
2024-11-05 16:55:44 -0500

    js Basic Functions

---
+++ javascript/variable.js
@@ -0,0 +1,8 @@
+let name = "Alice";
+let age = 25;
+let isStudent = true;
+let hobbies = ["reading", "coding"];
+
+console.log(name + " is " + age + " years old.");
+console.log("Is a student:", isStudent);
+console.log("Hobbies:", hobbies.join(", "));
