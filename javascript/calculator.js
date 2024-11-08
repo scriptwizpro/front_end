8ea2a57de1b803b38d8c484a9ec7d71e6b37e39f
Dev <androidsoloapps@gmail.com>
2024-11-08 16:37:58 -0500

    js Simple Calculator

---
+++ javascript/calculator.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+function subtract(a, b) {
+  return a - b;
+}
+function multiply(a, b) {
+  return a * b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+console.log("Add:", add(2, 3));
+console.log("Subtract:", subtract(5, 2));
+console.log("Multiply:", multiply(4, 5));
+console.log("Divide:", divide(10, 2))
